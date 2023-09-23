2fd2c8cc258bef21ac60967401b3447645ffa46a
Krishna  Patel <krishna.patel@avoltacanada.com>
2023-09-23 17:49:10 -0400

    activity 3b

--- hello.py
+++ hello.py
@@ -1,6 +1,9 @@
 from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
+
+bootstrap = Bootstrap(app)
 
 
 @app.route('/')
@@ -11,4 +14,3 @@ def index():
 @app.route('/user/<name>')
 def user(name):
     return render_template('user.html', name=name)
-
